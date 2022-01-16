d9c40faf10711b10f13efe3515cba51609698bb7
preprocessor <preprocessor@users.noreply.github.com>
2022-01-15 19:51:55 -0500

    Version1

---
+++ index.js
@@ -0,0 +1,7 @@
+function glitchText() {
+  glitchRef = document.getElementsByClassName("glitch")[0];
+
+  for (i=0; i < 9; i++) {
+    glitchRef.innerHTML += "<div class='line'>wyspr.xyz</div>";
+  }
+}

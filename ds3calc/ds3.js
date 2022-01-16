aa4a49ea4d0e539d0900b9f17583d04a7ab0c321
preprocessor <preprocessor@users.noreply.github.com>
2022-01-16 01:56:45 -0500

    more ds3calc polish

--- ds3calc/ds3.js
+++ ds3calc/ds3.js
@@ -1,19 +1,20 @@
 function level(lv) {
   let x = lv + 1
-  /* The level formula wants the target level, but our math gives the current level, so we +1
-   * I got this from the wiki but some levels seem to be off by a few souls                 */
+  /* The level formula wants the target level, but our math gives the current level, so we +1 *
+   * I got this math from the wiki but some levels seem to be off by a few souls              */
   return Math.floor( 0.02 * x **3 + 3.06 * x **2 + 105.6 * x - 895 );
 }
 
 function levelMap(start, end) {
   let results = new Map();
-  /*
-   *  The level formula is innacurate at levels < 12,
-   *  so instead we put lvs 1-12 into an array and
-   *  loop through those when < 12
-   *               ┌─array indexes start at 0 but our loop counts from 1 so we insert a 0 at the start to shift the index +1 */
+  /* * * * * * * * * * * * * * * * * * * * * * * * * *
+   * The level formula is innacurate at levels < 12, *
+   * so instead we put lvs 1-12 into an array and    *
+   * loop through those when < 12                    *
+   * * * * * * * * * * * * * * * * * * * * * * * * * */
   let earlyGame = [0, 673, 689, 706, 723, 740, 757, 775, 793, 811, 829, 847]
-
+                /* └─array indexes start at [0] but our loop counts from 1 *
+                 *   so we insert a 0 at the start to shift the index up 1 */
   for (let i = start; i < end; i++) {
     if (i < 12 ) {
       results.set(i, earlyGame[i]);
@@ -26,9 +27,9 @@ function levelMap(start, end) {
 }
 
 function addRow(c1, c2, table) {
-  let newRow = table.insertRow(-1); // create a new row at the end of the table
-  let cell1 = newRow.insertCell(0);
-  let cell2 = newRow.insertCell(1); // insert the cells into the row
+  let newRow = table.insertRow(-1), // create a new row at the end of the table
+      cell1 = newRow.insertCell(0),
+      cell2 = newRow.insertCell(1); // insert the cells into the row
 
   cell1.innerHTML = c1;
   cell2.innerHTML = c2;             // populate the cells
@@ -41,21 +42,20 @@ function tableWipe(table) {
 }
 
 function tableGen() {
-  var start = parseInt(document.getElementById("startLvl").value);
-  var end   = parseInt(document.getElementById("goalLvl").value);
   // using parseInt() removes decimals and returns NaN if given a string
-
-  var tableRef = document.getElementById('daTable');
+  var start = parseInt(document.getElementById("startLvl").value),
+      end   = parseInt(document.getElementById("goalLvl").value),
+      tableRef = document.getElementById('daTable');
 
   if ( start && end && start >= 1 && start < end && end <= 802 ) { // makes sure that inputs exist and make sense
-    tableRef.style.display = 'table';                              // unhide the table on 1st run
-    tableWipe(tableRef);                                           // clear it in case its populated
+    tableRef.style.display = 'table';                              // unhide the table on first run
+    tableWipe(tableRef);                                           // clear the table if its populated
 
-    var values = levelMap(start, end);
-    var total = 0;
+    let values = levelMap(start, end),
+        total = 0;
 
     for (let [id, value] of values) {
-      total = total + value;
+      total += value;
       addRow(id + "<span>→</span>" + (id+1), value.toLocaleString(), tableRef);
     }
 

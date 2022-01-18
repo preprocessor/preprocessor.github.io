e252f27b8c2185a829721e65d7f8108242853e2c
preprocessor <preprocessor@users.noreply.github.com>
2022-01-17 22:41:57 -0500

    terminal mostly complete and now viewable

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function glitchText() {
 function treeFill() {
   let links = new Map([
     ['ds3-level-calculator', './ds3calc/'],
-    ['src',               'https://github.com/preprocessor/preprocessor.github.io']
+    ['src',                  'https://github.com/preprocessor/preprocessor.github.io']
   ]);//src needs to be the last one for the tree to work properly 🤪
 
   var treeRef = document.getElementsByClassName('tree')[0],
@@ -25,6 +25,41 @@ function treeFill() {
 
     treeRef.innerHTML += '<div class="treeItem">&nbsp;&nbsp;'+symbol+'<a href="'+link+'">'+name+'</a></div>';
   }
+}
+
+async function openTerminal() {
+  var terminal = document.getElementsByClassName('terminal')[0],
+      prompt   = document.getElementsByClassName('prompt'),
+      tree     = document.getElementsByClassName('tree')[0];
+
+
+  terminal.style = '';
+  terminal.classList.add('opened');
+
+  await waitForMs(1600);
+
+  await typeSentence("tree ~/xyz/pages", prompt[1]);
+
+  await waitForMs(300);
+
+  prompt[1].classList.remove('blinking');
+  prompt[1].classList.add('ran');
+  await waitForMs(100);
+  tree.style.opacity = 1;
+  await waitForMs(100);
+  prompt[2].classList.add('blinking');
+}
+
+async function typeSentence(sentence, ref, delay = 75) {
+  for (i=0; i < sentence.length; i++) {
+    ref.innerHTML += sentence.split("")[i];
+    await waitForMs(delay);
+  }
+  return;
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 function init() {

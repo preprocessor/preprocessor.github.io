8490fa83630807057725ebe7f4d1d58cbd49701f
preprocessor <preprocessor@users.noreply.github.com>
2022-01-22 18:49:38 -0500

    Major code refactor + comments

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 function glitchText() {
-  var glitchRef = document.getElementsByClassName('glitch')[0],
-      message   = 'wyspr.xyz'
-
   for (i=0; i < 9; i++) {
-    glitchRef.innerHTML += '<div class="line">' + message + '</div>';
+    items.glitchRef.innerHTML += '<div class="line">wyspr.xyz</div>';
   }
 }
 
 function treeFill() {
+  var symbol;
   let links = new Map([
     ['ds3-level-calculator', './ds3calc/'],
     ['src',                  'https://github.com/preprocessor/preprocessor.github.io']
   ]);//src needs to be the last one for the tree to work properly 🤪
-
-  var treeRef = document.getElementsByClassName('tree')[0],
-      symbol;
 
   for (let [name, link] of links) {
     if (name != 'src') {
@@ -23,41 +18,28 @@ function treeFill() {
       symbol = '└─';
     }
 
-    treeRef.innerHTML += '<div class="treeItem">&nbsp;&nbsp;'+symbol+'<a href="'+link+'">'+name+'</a></div>';
+    items.treeRef.innerHTML += '<div class="treeItem">&nbsp;&nbsp;'+symbol+'<a href="'+link+'">'+name+'</a></div>';
   }
+  return;
 }
 
 async function openTerminal() {
 
-  var glitch       = document.getElementsByClassName('glitch')[0],
-      terminal     = document.getElementsByClassName('terminal')[0],
-      titlebar     = document.getElementsByClassName('titleBar')[0],
-      tree         = document.getElementsByClassName('tree')[0],
+  var titlebar     = document.getElementsByClassName('titleBar')[0],
       prompts      = document.getElementsByClassName('prompt'),
       activePrompt = prompts[1],
       message      = prompts[0].innerText;
 
-  glitch.removeAttribute("onclick");
-  glitch.style.cursor = 'unset'
-  terminal.style = '';
-  terminal.classList.add('opened');
 
-  await waitForMs(1600);
-  titlebar.classList.add('revealed');
-  await waitForMs(156);
-  activePrompt.classList.add('blinking');
-  await waitForMs(364);
-  await typeSentence(message, activePrompt);
-  await waitForMs(300);
+  items.glitchRef.outerHTML = items.glitchRef.outerHTML.replace(/style=\"[^"]*" onclick=\"[^"]*"/, '')                       // removes ? cursor and onclick event from glitch text
+  items.terminal.classList.add('opened');                                                                                    // open terminal
 
-  activePrompt.classList.remove('blinking');
-  activePrompt.classList.add('ran');
-  await waitForMs(100);
-
-  tree.style.opacity = 1;
-  await waitForMs(100);
-
-  prompts[2].classList.add('blinking');
+  await waitForMs(randInterval(1500,1700)).then(()                  => {titlebar.classList.add('revealed')});                // reveal titlebar
+  await waitForMs(randInterval(100, 200)).then(()                   => {activePrompt.classList.add('blinking')});            // add cursor to prompt
+  await waitForMs(randInterval(300, 400)).then(()                   => {typeSentence(message, activePrompt)});               // type some words
+  await waitForMs(randInterval(250, 350)+message.length*75).then(() => {activePrompt.classList.replace('blinking', 'ran')}); // wait until message is finished (message.length*75) and remove the cursor
+  await waitForMs(randInterval(100, 200)).then(()                   => {items.treeRef.style.opacity = 1});                   // reveal the tree
+  await waitForMs(randInterval(100, 200)).then(()                   => {prompts[2].classList.add('blinking')});              // add cursor to next prompt
 }
 
 async function typeSentence(sentence, ref, delay = 75) {
@@ -72,7 +54,17 @@ function waitForMs(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function randInterval(min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+}
+
+var items = {};
+
 function init() {
+  items.glitchRef = document.getElementsByClassName('glitch')[0];
+  items.treeRef   = document.getElementsByClassName('tree')[0];
+  items.terminal  = document.getElementsByClassName('terminal')[0];
   glitchText();
   treeFill();
+  return;
 }

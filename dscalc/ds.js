50f0511c1960d280e441deaecf7c05211dff8bfd
preprocessor <preprocessor@users.noreply.github.com>
2022-02-01 21:32:57 -0500

    Added DS 1 and 2 to the calculator

--- dscalc/ds.js
+++ dscalc/ds.js
@@ -1,25 +1,33 @@
-function level(lv) {
-  let x = lv + 1
-  /* The level formula wants the target level, but our math gives the current level, so we +1 *
-   * I got this math from the wiki but some levels seem to be off by a few souls              */
-  return Math.floor( 0.02 * x **3 + 3.06 * x **2 + 105.6 * x - 895 );
+function level(lv, game) {
+  /* The level formula wants the target level, but our math gives the current level, so we +1 */
+  let x = lv + 1,
+      result;
+
+  if (game != 2) {
+    result = Math.floor( 0.02 * x **3 + 3.06 * x **2 + 105.6 * x - 895 );
+  } else {
+    result = ds2_levels[x]
+  }
+
+  return result;
 }
 
-function levelMap(start, end) {
+function levelMap(start, end, game) {
   let results = new Map();
   /* * * * * * * * * * * * * * * * * * * * * * * * * *
-   * The level formula is innacurate at levels < 12, *
-   * so instead we put lvs 1-12 into an array and    *
-   * loop through those when < 12                    *
+   * The level formula is innacurate at levels < 12  *
+   * for 1 and 3 so instead we put lvs 1-12 into an  *
+   * array and loop through those when < 12          *
    * * * * * * * * * * * * * * * * * * * * * * * * * */
   let earlyGame = [0, 673, 689, 706, 723, 740, 757, 775, 793, 811, 829, 847]
                 /* └─array indexes start at [0] but our loop counts from 1 *
                  *   so we insert a 0 at the start to shift the index up 1 */
+
   for (let i = start; i < end; i++) {
-    if (i < 12 ) {
+    if (i < 12 && game != 2) {
       results.set(i, earlyGame[i]);
     } else {
-      results.set(i, level(i));
+      results.set(i, level(i, game));
     }
   }
 
@@ -36,35 +44,47 @@ function addRow(c1, c2, table) {
 }
 
 function tableWipe(table) {
-  while ( table.rows.length > 1) {
+  while (table.rows.length > 1) {
     table.deleteRow(-1);
   }
 }
 
 function tableGen() {
-  // using parseInt() strips decimals and returns NaN if given a string
-  var start    = parseInt(document.getElementById("startLvl").value),
-      end      = parseInt(document.getElementById("goalLvl").value),
-      tableRef = document.getElementById('daTable');
+  var start    = parseInt(document.getElementById('startLvl').value),
+      end      = parseInt(document.getElementById('goalLvl').value),
+      //   using parseInt() strips decimals and returns NaN if given a string
+      tableRef = document.getElementById('daTable'),
+      radios   = document.querySelectorAll('input[name="game"]'),
+      game;
+
+  for (const radioButton of radios) {
+    if (radioButton.checked) {
+      game = radioButton.value;
+      break;
+    }
+  }
+
 
   if ( start && end && start >= 1 && start < end && end <= 802 ) { // makes sure that inputs exist and make sense
     tableRef.style.display = 'table';                              // unhide the table on first run
     tableWipe(tableRef);                                           // clear the table if its populated
 
-    let values = levelMap(start, end),
+    let levels = levelMap(start, end, game),
         total  = 0;
 
-    for (let [id, value] of values) {
+    for (let value of levels.values()) {
       total += value;
-
-      addRow(id + "<span>→</span>" + (id+1), value.toLocaleString(), tableRef);
     }
 
-    addRow("Total Souls", total.toLocaleString(), tableRef);
+    for (let [id, value] of levels) {
+      addRow(id + '<span>→</span>' + (id+1), value.toLocaleString(), tableRef);
+    }
+
+    addRow('Total Souls', total.toLocaleString(), tableRef);
   }
 }
 
-function enterSubmit () {
+function enterSubmit() {
   document.querySelectorAll('input').forEach(item => {
     item.addEventListener('keyup', (e) => {
       if (e.key === 'Enter') {tableGen();}
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function glitchText() {
-  for (i=0; i < 9; i++) {
+  for (i = 0; i < 9; i++) {
     items.glitchRef.innerHTML += '<div class="line">wyspr.xyz</div>';
   }
+  return;
 }
 
 function treeFill() {
   var symbol;
   let links = new Map([
-    ['ds3-level-calculator', './ds3calc/'],
+    ['ds-level-calculator', './dscalc/'],
     ['src',                  'https://github.com/preprocessor/preprocessor.github.io']
   ]);//src needs to be the last one for the tree to work properly 🤪
 
@@ -30,7 +31,6 @@ async function openTerminal() {
       activePrompt = prompts[1],
       message      = prompts[0].innerText;
 
-
   items.glitchRef.outerHTML = items.glitchRef.outerHTML.replace(/style=\"[^"]*" onclick=\"[^"]*"/, '')                       // removes ? cursor and onclick event from glitch text
   items.terminal.classList.add('opened');                                                                                    // open terminal
 
@@ -40,10 +40,12 @@ async function openTerminal() {
   await waitForMs(randInterval(250, 350)+message.length*75).then(() => {activePrompt.classList.replace('blinking', 'ran')}); // wait until message is finished (message.length*75) and remove the cursor
   await waitForMs(randInterval(100, 200)).then(()                   => {items.treeRef.style.opacity = 1});                   // reveal the tree
   await waitForMs(randInterval(100, 200)).then(()                   => {prompts[2].classList.add('blinking')});              // add cursor to next prompt
+
+  return;
 }
 
 async function typeSentence(sentence, ref, delay = 75) {
-  for (i=0; i < sentence.length; i++) {
+  for (i = 0; i < sentence.length; i++) {
     ref.innerHTML += sentence.split("")[i];
     await waitForMs(delay);
   }
